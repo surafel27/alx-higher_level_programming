c24b18c53d13899532131a4d5562d8f1e4947081
surafel27 <surafelfekadu399@gmail.com>
2022-11-28 01:31:58 -0800

    python is easy

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,2 @@
+#this line prints the statemen
+print("\"Programming is like building a multilingual puzzle")
---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+number = 98
+txt = 'Battery street'
+print(f"{number} {txt}")
