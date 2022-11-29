67679dcad515b457c394bad7925ceb5d1671dd31
surafel27 <surafelfekadu399@gmail.com>
2022-11-29 05:30:40 -0800

    find last digit of the rand num

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 lastnum = int(repr(number)[-1])
 if lastnum > 5:
-    print("last digit of {} is {} and is greater than 5".format(number, lastnum))
+    print("Last digit of {} is {} and is greater than 5".format(number, lastnum))
 elif lastnum == 0:
-    print("last digit of {} is {} and is 0".format(number, lastnum))
+    print("Last digit of {} is {} and is 0".format(number, lastnum))
 elif lastnum < 6:
-    print("last digit of {} is {} and is lessthan than 6 and not 0".format(number, lastnum))
+    print("Last digit of {} is {} and is lessthan than 6 and not 0".format(number, lastnum))
 
