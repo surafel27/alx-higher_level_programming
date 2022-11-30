64c7b66baf4c2cc9b44fe2aace0203636e6e903f
surafel27 <surafelfekadu399@gmail.com>
2022-11-30 01:32:41 -0800

    implement ur own UPPER()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    str = str.upper()
-    print("{}".format(str))
+    for x in str:
+        if ord(x) > 96 and ord(x) < 123:
+            x = chr(ord(x) - 32)
+        print('{}'.format(x), end='')
+    print("")
