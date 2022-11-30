58a337a1d81d9133a914f03a456a47a9f6b7f225
surafel27 <surafelfekadu399@gmail.com>
2022-11-29 21:56:12 -0800

    print last digit of randome number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastnum = int(repr(number)[-1])
+if number < 0:
+    lastnum = - 1 * (int(repr(-number)[-1]))
+else:
+    lastnum = int(repr(number)[-1])
+
 if lastnum > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, lastnum))
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, lastnum))
 elif lastnum == 0:
-    print("Last digit of {} is {} and is 0".format(number, lastnum))
+    print("Last digit of {:d} is {:d} and is 0".format(number, lastnum))
 elif lastnum < 6:
-    print("Last digit of {} is {} and is lessthan than 6 and not 0".format(number, lastnum))
+    print("Last digit of {:d} is {:d} and is lessthan than 6 and not 0".format(number, lastnum))
 
