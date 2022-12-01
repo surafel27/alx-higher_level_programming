dd27d50a6e45b564deffe53a3c3e4814155183ae
surafel27 <surafelfekadu399@gmail.com>
2022-12-01 04:12:48 -0800

    programm takes argumnts and print thier index and the argument it self

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print(" 0 arguments.")
-    elif len(sys.argv) >= 2:
-        print("{} arguments:".format(len(sys.argv)))
+    j = len(sys.argv) - 1
+    if j < 2:
+        print("{} arguments.".format(j))
+    elif j >= 2:
+        print("{} arguments:".format(j))
         for i, arg in enumerate(sys.argv):
             if i == 0:
                 continue
