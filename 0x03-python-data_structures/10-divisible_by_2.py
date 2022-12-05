3e951e8b1f3887dba23fdf2bd9ab832330f77f7d
surafel27 <surafelfekadu399@gmail.com>
2022-12-05 02:22:53 -0800

    check list of number to divided for 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    for i in my_list:
-        if my_list[i] % 2 == 0:
+    for j in my_list:
+        if j % 2 == 0:
             new_list.append(True)
-        elif my_list[i] % 2 != 0:
+        else:
             new_list.append(False)
     return (new_list)
