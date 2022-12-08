b8c787b117b755a8ff9b48ad0522d6f4567a4ffa
surafel27 <surafelfekadu399@gmail.com>
2022-12-07 22:13:28 -0800

    replace the item of list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for item in my_list:
-        if item != search:
-            return item
-        else:
-            return replace
+    return[item if item != search else replace for item in my_list]
