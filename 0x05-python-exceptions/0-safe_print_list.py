aea8460a8585f45dbee354e50c67b9b008c6f852
surafel27 <surafelfekadu399@gmail.com>
2022-12-19 01:56:11 -0800

    safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    result = 0
+    for idx in range(x):
+        try:
+            print("{}".format(my_list[idx]), end="")
+            result += 1
+        except IndexError:
+            break
+    print()
+    return(result)
