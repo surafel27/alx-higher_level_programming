def222b79db9d30968110165c3295e242771042c
surafel27 <surafelfekadu399@gmail.com>
2023-01-03 00:13:30 -0800

    lazy multiplication

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 101-lazy_matrix_mul
+Defines lazy_matrix_mul
+multiplication of matrixes
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Return the multiplication
+    """
+    return numpy.matmul(m_a, m_b)
