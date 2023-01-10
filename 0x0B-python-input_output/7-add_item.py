816470df624f35845a177914d777376db6a69346
surafel27 <surafelfekadu399@gmail.com>
2023-01-10 05:00:23 -0800

    load, add and save JSON file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """function load, add , save json file"""
 
-import json
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
