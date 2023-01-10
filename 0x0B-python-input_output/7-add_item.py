90a2b2cfffaa067273b1ee0d7c3fd8dd092023f5
surafel27 <surafelfekadu399@gmail.com>
2023-01-10 04:57:18 -0800

    load, add and save JSON file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,12 +2,11 @@
 """function load, add , save json file"""
 
 import json
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 try:
-    loadFile = load_from_json_file(add_item.json)
+    loadFile = load_from_json_file("add_item.json")
 except FileNotFoundError:
     loadFile = []
 argc = len(sys.argv)
