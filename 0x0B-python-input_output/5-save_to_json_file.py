d7371a204f766ff8c5be44c120255c863b606195
surafel27 <surafelfekadu399@gmail.com>
2023-01-10 04:18:24 -0800

    writing on JSON file representation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ def save_to_json_file(my_obj, filename):
     """takes the object and write to json string"""
     with open(filename, mode="w", encoding="utf-8") as myFile
     my_obj = json.dump(my_obj)
-    myfile.write(my_obj)
+    myFile.write(my_obj)
