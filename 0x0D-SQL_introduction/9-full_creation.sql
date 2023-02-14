7273ae667eba75537a493304995fc235d31c879b
surafel27 <surafelfekadu399@gmail.com>
2023-02-14 12:14:15 -0800

    full creation of table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- Full creation of table with attribute and valus.
+-- Full creation of table with attribute and valus to the table.
 CREATE TABLE IF NOT EXISTS `second_table`(`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "JHON", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
