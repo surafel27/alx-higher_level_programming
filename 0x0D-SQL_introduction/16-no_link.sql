bc7a5f2b6db872275d79c75efa360af085dd1bf6
surafel27 <surafelfekadu399@gmail.com>
2023-02-14 23:18:53 -0800

    Exists the name print the score and the name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- List the score and the name if only name exist.
-SELECT `score`, `name` FROM `second_table` WHERE EXISTS (SELECT `name` FROM `second_table`) ORDER BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
