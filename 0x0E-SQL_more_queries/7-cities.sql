0f1268284cf1d5bf98624438449c44969e6ef5d2
surafel27 <surafelfekadu399@gmail.com>
2023-02-15 04:06:00 -0800

    forign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- create database then table, make id unique, auto generated, an primary key.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `cities`(`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, `state_id` INT, FOREIGN KEY(`state_id`) REFERENCES `states`(`id`), `name` VARCHAR(256) NOT NULL);
