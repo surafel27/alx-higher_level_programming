1b5335e47a7cc6338cc8b2520b68e5cfa5c3f194
surafel27 <surafelfekadu399@gmail.com>
2023-02-15 04:41:50 -0800

    using left join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- List cities of ALL CITIES in database.
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.id = states.id ORDER BY cities.id;
