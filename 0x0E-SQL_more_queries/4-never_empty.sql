e85bbd2a9c9f202a42c15548bb762ad024f57524
surafel27 <surafelfekadu399@gmail.com>
2023-02-15 03:22:06 -0800

    create table and make ID attribute to default to 1

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
--- Create table.
-CREATE TABLE IF NOT EXISTS `force_name`
+-- Create table with the id Attribute set to default 1.
+CREATE TABLE IF NOT EXISTS `id_not_null`(`id` INT DEFAULT 1, `name` VARCHAR(256));
