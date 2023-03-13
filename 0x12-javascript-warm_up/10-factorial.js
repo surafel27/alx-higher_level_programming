a5ef3ca7a1072d7e6ec0b3094e2c32702fd223e3
root <surafel27>
2023-03-13 13:15:38 +0000

    fix for checker 5

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 function factorial(num) {
-if (num === 0 || isNaN(num)) {
-   return 1;
-}
+  if (num === 0 || isNaN(num)) {
+    return 1;
+  }
   return num * factorial(num - 1);
 }
 const factNumber = parseInt(process.argv[2]);
