25abee49da0688e12dd8684b342da75a96070223
root <surafel27>
2023-03-13 12:04:03 +0000

    fix number 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const strNumber = parseInt(process.argv[2]);
-if (!strNumber) {
-  console.log('Not a number');
+if (strNumber) {
+  console.log('My number: ' +  strNumber);
 }else {
-  console.log('My number:' + ' ' + strNumber);
+  console.log('Not a number');
 }
