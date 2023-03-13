e9f7770727cb5223167898342372656c9de78dff
root <surafel27>
2023-03-13 10:46:03 +0000

    casting string to number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@
 if (!Number(process.argv[2])) {
   console.log('Not a number');
 }else {
-  console.log('My number:'+ ' ' + Number(process.argv[2]));
+  console.log('My number:' + ' ' + Number(process.argv[2]));
 }
