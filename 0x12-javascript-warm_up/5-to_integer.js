f4127cac4b044e2f23a39a8e191023cd85ce710b
root <surafel27>
2023-03-13 11:56:52 +0000

    fixing number 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (!Number(process.argv[2])) {
+if (!parseInt(process.argv[2])) {
   console.log('Not a number');
 }else {
-  console.log('My number:' + ' ' + Number(process.argv[2]));
+  console.log('My number:' + ' ' + parseInt(process.argv[2]));
 }
