2a18b5abbe256a7603521119450394c5e74bab21
root <surafel27>
2023-03-13 11:51:40 +0000

    multi arg and print C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,6 +4,7 @@ const strNumber = parseInt(process.argv[2]);
 if (!strNumber) {
   console.log('Missing number of occurrences');
 } else {
- for (let i = 0; i < strNumber; ++i) {
-     console.log('C is fun');
+  for (let i = 0; i < strNumber; ++i) {
+    console.log('C is fun');
+  }
 }
