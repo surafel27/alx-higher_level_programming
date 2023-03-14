f4aeb4101bba4b653971a6058ddd4c102aa02e69
root <surafel27>
2023-03-14 10:55:53 +0000

    fixing error

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 //write an empty class called Rectangle
 
 module.exports = class Rectangle { };
--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+
 //write class called Rectangle
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
