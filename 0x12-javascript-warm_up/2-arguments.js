a185e98ad8028cb650abe22e922f7f3c71e3a277
root <surafel27>
2023-03-14 07:11:29 +0000

    semistandard for the Js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 if (process.argv.length > 3) {
   console.log('Arguments found');
 } else if (process.argv.length === 3) {
-  console.log('Arguments found');
+  console.log('Argument found');
 } else {
   console.log('No argument');
 }
