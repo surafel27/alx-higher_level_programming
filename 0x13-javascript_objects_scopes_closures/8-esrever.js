c5568ceca12b08cd969f57881f4ea4a0c2aeea1a
root <surafel27>
2023-03-15 11:40:06 +0000

    semistandard

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let newarry = [];
-  for (let i = list.length - 1; i >=0; i--) {
+  const newarry = [];
+  for (let i = list.length - 1; i >= 0; i--) {
     newarry.push(list[i]);
   }
   return newarry;
