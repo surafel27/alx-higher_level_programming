418b2d48624e61cfba8e73bedccb1b14847d4523
root <surafel27>
2023-03-15 09:53:10 +0000

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+//write class called Rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    let string = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j <this.width; j++) {
+        string += 'X';
+      }
+      if (i === this.height - 1) {
+        break;
+      }
+      string += '\n';
+    }
+    console.log(string);
+  }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
