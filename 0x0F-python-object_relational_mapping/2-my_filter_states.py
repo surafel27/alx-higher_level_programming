3155b094a156b5b87bcfe0bcc7297ac8c8eb65b7
surafel27 <surafefekadu399@gmail.com.com>
2023-03-17 01:11:06 -0700

    use f string to these task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+List all states from the database
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    names = sys.argv[4]
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name = '{names}' ORDER BY states.id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
