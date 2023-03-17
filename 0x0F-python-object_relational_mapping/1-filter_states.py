00a0bcde5cfd2897ba73ec94a7786be6376ecf0b
surafel27 <surafefekadu399@gmail.com.com>
2023-03-17 01:21:47 -0700

    fixing error number one

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
     states = cur.fetchall()
     for state in states:
-        print(state)
+        if state[1][0] == 'N':
+            print(state)
     cur.close()
     db.close()
