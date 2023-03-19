b891fbc713be4411b503ff0fe788ea94143eaf6b
surafel27 <surafefekadu399@gmail.com.com>
2023-03-19 03:14:15 -0700

    listing cities with left join

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+List all states from the database
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                 LEFT JOIN states ON cities.id = states.id \
+                 ORDER BY cities.id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
