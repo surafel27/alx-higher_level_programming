e98b84bc07ab739d0eca58fd58379aa6f2c28ee2
surafel27 <surafefekadu399@gmail.com.com>
2023-03-19 09:34:06 -0700

    delete all

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+import sqlalchemy
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """connectin to the database"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+             argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    stmnt = session.query(State).filter(State.name.like('%a%')).all()
+    for st in stmnt:
+        session.delete(st)
+    session.commit()
+    session.close()
