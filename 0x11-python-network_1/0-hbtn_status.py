f963a3b6a3187da4358eb892b1bd9ce1bd9df946
surafel27 <surafefekadu399@gmail.com.com>
2023-03-31 03:20:41 -0700

    script that fetches link address

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,8 @@ from urllib import request
 
 req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
-	the_response = response.read()
-	html = the_response.decode('UTF-8')
+    the_response = response.read()
+    html = the_response.decode('UTF-8')
 print("- type:", type(the_response))
 print("- content:", the_response)
 print("- utf8 content:", html)
