8702be1e5f3ce31493be5da8df30ae193a68fa1c
surafel27 <surafefekadu399@gmail.com.com>
2023-03-31 03:27:27 -0700

    script that fetches link address

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alx-intranet.hbtn.io/status """
-import urllib
-from urllib import request
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    the_response = response.read()
-    html = the_response.decode('UTF-8')
-print("- type:", type(the_response))
-print("- content:", the_response)
-print("- utf8 content:", html)
+if __name__ == "__main__":
+    import urllib
+    from urllib import request
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        the_response = response.read()
+        html = the_response.decode('UTF-8')
+        print("Body response:")
+        print("\t - type:", type(the_response))
+        print("\t - content:", the_response)
+        print("\t - utf8 content:", html)
