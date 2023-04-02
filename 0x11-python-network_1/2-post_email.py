46e144f34d3aba100350903c3a85623cd612b1d6
surafel27 <surafefekadu399@gmail.com.com>
2023-04-02 00:01:55 -0700

    sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,6 +2,7 @@
 import urllib
 import sys
 from urllib import request, parse
+
 """ sends a POST request to the passed URL with the email
     - takes url
     - send a post request
@@ -9,13 +10,14 @@ from urllib import request, parse
     - display the reasponse in utf-8 format
 """
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
     val = {"email": sys.argv[2]}
     query_string = urllib.parse.urlencode(val)
     data = query_string.encode("ascii")
 
-    req = urllib.request.Request(url, data)
+    req = urllib.request.Request(url=url, data=data)
     with urllib.request.urlopen(req) as response:
         response_text = response.read()
         print(response_text.decode("utf-8"))
