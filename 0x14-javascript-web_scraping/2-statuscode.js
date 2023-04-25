a351a3cccd1f204d57828b829040fd23c8deafcd
surafel27 <surafefekadu399@gmail.com.com>
2023-04-25 04:53:27 -0700

    script that display the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 const request = require('request');
 const process = require('process');
 const url = process.argv[2];
-const res = request(url);
+const res = request.get(url);
 res.on('error', function (error) {
   console.log(error);
 });
 res.on('response', function (response) {
-  console.log('code: ', response && response.statusCode);
+  console.log('code:', response.statusCode);
 });
